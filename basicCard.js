6caac4c0bc742acab274cf7cefbbb852691c616b
unknown <thompson.jonm@gmail.com>
2017-07-17 15:12:34 -0500

    further edits

--- basicCard.js
+++ basicCard.js
@@ -1,80 +1,23 @@
-// require inquirer
-var inquirer = require("inquirer");
 
-//importat cards
-var flashCards = require("./basicCard.js");
-
-//import cloze
-var clozeCards = require("./clozeCard.js");
-
-//variable for the questions list
-var clozeQuestions = [];
-
-//RNG population
-for (var i = 0; i < questions.length; i++) {
-    var q = new flashCards.clozeCard(clozeCards[i].full, clozeCards[i].cloze);
-    clozeQuestions.push[q];
+//basic card constructor
+exports.BasicCard = function(front, back) {
+	this.front = front;
+	this.back = back;
 }
 
-//current answer counter
-var currentQuestion = 0;
+//cloze card constructor
+exports.ClozeCard = function(text, cloze) {
+	// Convert the incoming strings to lower case
+	var textToLower = text.toLowerCase();
+	var clozeToLower = cloze.toLowerCase();
 
-//correct answer counter
-var correctAnswer = 0;
+	//cloze statement validation
+	if (!textToLower.includes(clozeToLower)) {
+		console.log('Cloze-deletion does not appear within full text -- <' + cloze + '>');
+		return;
+	}
 
-//wrong answer counter
-var wrongAnswer = 0;
-
-function askQuestion() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: clozeQuestions[currentQuestion].partial + "\nAnswer: ",
-            name: "userGuess"
-        }
-    ]).then(function(answers) {
-        console.log("\n");
-
-        if (answers.userGuess.toLowerCase() === clozeQuestions[currentQuestion].cloze.toLowerCase()) {
-            console.log("You are correct!");
-            correctAnswer++;
-        } else {
-            console.log("You are incorrect.");
-            wrongAnswer++;
-        }
-
-        console.log(clozeQuestions[currentQuestion].full);
-        console.log("-------------------------------\n");
-
-        if (currentQuestion < clozeQuestions.length - 1) {
-            currentQuestion++;
-            askQuestion();
-        } else {
-            console.log("Game over!");
-            console.log("Number of correct answers: " + correctAnswer);
-            console.log("Number of incorrect answers: " + wrongAnswer);
-
-            console.log("-------------------------------\n");
-
-            inquirer.prompt([
-                {
-                    type: "confirm",
-                    message: "Play again?",
-                    name: "playAgain"
-                }
-            ]).then(function(answers) {
-                if(answers.playAgain) {
-                    currentQuestion = 0;
-                    correctAnswer = 0;
-                    wrongAnswer = 0;
-
-                    askQuestion();
-                } else {
-                    console.log("Alrighty then! Goodbye!");
-                }
-            })
-        }
-    })
+	this.full = text;
+	this.cloze = cloze;
+	this.partial = text.replace(cloze, '...');
 }
-
-askQuestion();
